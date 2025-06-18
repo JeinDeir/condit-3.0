b2716bfc7abfe6a4d6179db2c1b85f63be07a739
JeinDeir <permyakovvitaliy398@gmail.com>
2025-06-18 19:03:14 +0900

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function showCategory(category) {
-    const categories = document.querySelectorAll('.menu-items');
-    categories.forEach(cat => cat.style.display = 'none');
-
-    document.getElementById(category).style.display = 'flex';
-}
